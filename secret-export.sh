0b867c22d24817437320c11eb9909f89b67eb19f
Yunusa M. Namadi <98776848+ynamadi@users.noreply.github.com>
2022-06-24 07:40:18 -0400

    Redeploying TAP full cluster with proxy config

---
+++ secret-export.sh
@@ -0,0 +1,18 @@
+cat <<EOF | kubectl delete -f -
+---
+apiVersion: secretgen.carvel.dev/v1alpha1
+kind: SecretExport
+metadata:
+  name: store-ca-cert
+  namespace: metadata-store-secrets
+spec:
+  toNamespaces: [tap-dev]
+---
+apiVersion: secretgen.carvel.dev/v1alpha1
+kind: SecretExport
+metadata:
+  name: store-auth-token
+  namespace: metadata-store-secrets
+spec:
+  toNamespaces: [tap-dev]
+EOF
