dca235527c5c2edd76996d310687c13cd8b1c796
Yunusa M. Namadi <98776848+ynamadi@users.noreply.github.com>
2022-12-06 14:33:56 -0500

    Creating Sct for cosign

--- test-metadata-store-connection.sh
+++ test-metadata-store-connection.sh
@@ -3,7 +3,7 @@ nslookup $METADATA_STORE_URL
 curl "https://${METADATA_STORE_URL}/api/health" -k -v
 
 kubectl get secret ingress-cert -n metadata-store -o json | jq -r '.data."ca.crt"' | base64 -d > insight-ca.crt
-tanzu insight config set-target ${METADATA_STORE_URL} --ca-cert insight-ca.crt
+tanzu insight config set-target "https://${METADATA_STORE_URL}" --ca-cert insight-ca.crt
 tanzu insight health
 #To retrieve the read-only access token, run the following command:
 export METADATA_STORE_ACCESS_TOKEN
