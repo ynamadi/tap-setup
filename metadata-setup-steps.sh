b76a5fc3224a4bc700353cc1251c959fe2d6bf44
Yunusa M. Namadi <98776848+ynamadi@users.noreply.github.com>
2023-02-10 10:07:35 -0500

    Metadata store setup

--- metadata-setup-steps.sh
+++ metadata-setup-steps.sh
@@ -1,8 +1,9 @@
-nslookup metadata-store.tanzu.y-compiles.com
-curl https://metadata-store.tanzu.y-compiles.com/api/health -k -v
+nslookup metadata-store.tap.tanzu.y-compiles.com
+curl https://metadata-store.tap.tanzu.y-compiles.com/api/health -k -v
 
 kubectl get secret ingress-cert -n metadata-store -o json | jq -r '.data."ca.crt"' | base64 -d > insight-ca.crt
-tanzu insight config set-target https://metadata-store.tanzu.y-compiles.com --ca-cert insight-ca.crt
+tanzu insight config set-target https://metadata-store.tap.tanzu.y-compiles.com --ca-cert insight-ca.crt
 tanzu insight health
 #To retrieve the read-only access token, run the following command:
-export METADATA_STORE_ACCESS_TOKEN=$(kubectl get secrets -n metadata-store -o jsonpath="{.items[?(@.metadata.annotations['kubernetes\.io/service-account\.name']=='metadata-store-read-client')].data.token}" | base64 -d)
+export METADATA_STORE_ACCESS_TOKEN=$(kubectl get secrets metadata-store-read-write-client -n metadata-store -o jsonpath="{.data.token}" | base64 -d)
+echo "$METADATA_STORE_ACCESS_TOKEN"
