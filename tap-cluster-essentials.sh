3c71536f47a4c63d3ff76267f5057b081a047a01
oladapooloyede <30477121+oladapooloyede@users.noreply.github.com>
2023-07-24 08:36:48 -0600

    Update tap-cluster-essentials.sh

--- tap-cluster-essentials.sh
+++ tap-cluster-essentials.sh
@@ -2,6 +2,6 @@
 
 export INSTALL_BUNDLE=registry.tanzu.vmware.com/tanzu-cluster-essentials/cluster-essentials-bundle@sha256:79abddbc3b49b44fc368fede0dab93c266ff7c1fe305e2d555ed52d00361b446
 export INSTALL_REGISTRY_HOSTNAME=registry.tanzu.vmware.com
-export INSTALL_REGISTRY_USERNAME=ynamadi@vmware.com
-export INSTALL_REGISTRY_PASSWORD='bIchef-tuvtu4-sutfyr'
+export INSTALL_REGISTRY_USERNAME=
+export INSTALL_REGISTRY_PASSWORD=
 ./install.sh --yes
