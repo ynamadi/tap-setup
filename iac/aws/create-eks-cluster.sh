396269f8918636ddaa093fdc4b36ceead52ab197
Yunusa M. Namadi <98776848+ynamadi@users.noreply.github.com>
2023-11-02 10:22:39 -0400

    Initial deployment of TAP v1.7.0-rc.19

--- iac/aws/create-eks-cluster.sh
+++ iac/aws/create-eks-cluster.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
-export EKS_CLUSTER_NAME=tap-full
-export AWS_REGION="us-west-1"
+export EKS_CLUSTER_NAME=aws-eks-us-east2
+export AWS_REGION="us-east-1"
 
 
 eksctl create cluster --name $EKS_CLUSTER_NAME --managed --region $AWS_REGION --instance-types t3.2xlarge --version 1.24 --with-oidc -N 4
